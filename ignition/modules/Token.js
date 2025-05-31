29d388be12ce9c96415c28e8812c70ffa084c23a
Katherine Weston <littlerobinagency@gmail.com>
2025-05-31 14:35:06 +0000

    add token.sol for smoke test

---
+++ ignition/modules/Token.js
@@ -0,0 +1,9 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("TokenModule", (m) => {
+
+  const token = m.contract("Token", [], {});
+   
+
+  return { token };
+});
