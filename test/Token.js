708abdd564fcfaf66882856f58cadc1f8fe0f1f0
Katherine Weston <littlerobinagency@gmail.com>
2025-07-10 09:23:18 +0000

    add testing contracts

--- test/Token.js
+++ test/Token.js
@@ -1,13 +1,34 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
+
+const { deployTokenFixture } = require("./helpers/TokenFixtures")
+
+const tokens = (n) => {
+    return ethers.parseUnits(n.toString(), 18)
+
+}
 
 describe("Token", () => {
 
     it("has correct name", async () => {
-        const Token = await ethers.getContractFactory("Token")
-        const token = await Token.deploy()
-        const name = await token.name()
-        expect(name).to.equal("My Token")
+        const { token } = await loadFixture(deployTokenFixture)
+        expect(await token.name()).to.equal("Dapp University")
+    })
+
+     it("has correct symbol", async () => {
+        const { token } = await loadFixture(deployTokenFixture)
+        expect(await token.symbol()).to.equal("DAPP")
+    })
+
+     it("has correct decimals", async () => {
+        const { token } = await loadFixture(deployTokenFixture)
+        expect(await token.decimals()).to.equal(18)
+    })
+
+     it("has correct total supply", async () => {
+        const { token } = await loadFixture(deployTokenFixture)
+        expect(await token.totalSupply()).to.equal(tokens("1000000"))
     })
 
 })
